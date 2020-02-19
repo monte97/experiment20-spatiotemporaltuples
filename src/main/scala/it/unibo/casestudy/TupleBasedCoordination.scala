2897604da403551d33ee9480b8c49ea176412d0f
Roberto Casadei <robyonrails@gmail.com>
2020-02-19 15:49:50 +0100

    Tuple-based coordination: advances using prolog tuples

--- src/main/scala/it/unibo/casestudy/TupleBasedCoordination.scala
+++ src/main/scala/it/unibo/casestudy/TupleBasedCoordination.scala
@@ -1,11 +1,13 @@
 package it.unibo.casestudy
 
+import alice.tuprolog.{Prolog, Theory}
 import it.unibo.alchemist.model.implementations.positions.Euclidean2DPosition
 import it.unibo.alchemist.model.scafi.ScafiIncarnationForAlchemist._
 import it.unibo.scafi.space.{Point2D, Point3D}
+
 import scala.collection.JavaConverters._
 
-class TupleBasedCoordination extends AggregateProgram with StandardSensors with Gradients with BlockG with ScafiAlchemistSupport
+class TupleBasedCoordination extends AggregateProgram with TupleSpace with StandardSensors with Gradients with BlockG with ScafiAlchemistSupport
   with FieldCalculusSyntax with FieldUtils with CustomSpawn with BlockS with BlockC {
   val tupleProcessPrefix = "tp_"
 
@@ -21,9 +23,62 @@ class TupleBasedCoordination extends AggregateProgram with StandardSensors with 
   def current2DPosition(): Point2D = Point2D(currentPosition().x, currentPosition().y)
 
   override def main(): Any = {
+    val task: Option[String] = solveWithMatch("task(T)", "T")
+    node.put("task", task)
+    val taskGenerator = mid % 2 == 0
+    val doGenerateTask = taskGenerator && task.isEmpty && mid == 10
+    node.put("doGenerateTask", doGenerateTask)
+    val doReadTask = !taskGenerator && task.isEmpty
+    node.put("doReadTask", doReadTask)
+
+    val p1 = out("task(x)", when = doGenerateTask)
+
+    val p2 = rd(tt = "task(X)", when = doReadTask)
+
+    node.put("p1", p1)
+    node.put("p2", p2)
+
+    /*
+    // From the beginning
+    outInRegion("out-in-circle", RectangularRegion(Point2D(88,38), Point2D(110,62)), Some("out2"))
+
+    branch(mid==1 && k==1) { out("hello", extension = 20+Math.random()*50, Some("out1")) }{}
+
+    branch(mid==152){ outHere("out-in-152", 12, Some("out3")) }{}
+
+    branch(mid==19 && k==20) { in("hello", Some("in1")) }{}
+
+    branch(mid==152 && k==150){ read("hello",Some("read1")) }{}
+
+    branch(mid==29 && k==170) { out("hello", extension = 20+Math.random()*50, Some("out1")) }{}
+
     spawn[Int,Int,Int](k => a => (k,true), Set(mid), 0)
       .continueWith[Int,Int,Double](k => a => (k+1.0, true), x => if(x==5) Some(x * 1000) else None, m => 0)
+     */
+
+    node.put("theory", tupleSpace.getTheory.toString)
   }
+
+  def rd(tt: String, when: Boolean): Map[TupleOpId,TupleResult] = {
+    val top = TupleOpId(s"rd_${mid}_${k}")(Read(tt, mid))
+    sspawn[TupleOpId,ProcArg,TupleResult](tupleOperation _, if(when) Set(top) else Set.empty, ProcArg())
+      .act((_, res) => addTupleIfNotAlreadyThere(res.datum))
+  }
+
+  def out(tuple: String, when: Boolean): Map[TupleOpId,TupleResult] = {
+    val top = TupleOpId("out"+k)(OutMe(tuple, mid, 20))
+    sspawn(tupleOperation _, if(when) Set(top) else Set.empty, ProcArg()).onExit(toid => toid.op match {
+      case OutHere(t,_) => removeTuple(t)
+      case _ =>
+    }).map(toid => {
+      toid._1.op match {
+        case OutMe(t,_,_) => addTupleIfNotAlreadyThere(t)
+        case _ =>
+      }
+      toid
+    })
+  }
+
 
   type Process[K,A,R] = K => A => (R, Boolean)
 
@@ -31,20 +86,18 @@ class TupleBasedCoordination extends AggregateProgram with StandardSensors with 
     def continueWith[K2,A2,R2](process: Process[K2,A2,R2], keyGen: K=>Option[K2], argGen: Map[K,V]=>A2): Map[K2,R2] = {
       spawn[K2,A2,R2](process, pout.keySet.flatMap(keyGen(_)), argGen(pout))
     }
+
+    def onExit(action: K => Unit): Map[K,V] = {
+      rep(pout){ oldMap => (oldMap--pout.keySet).keySet.foreach(action); pout }
+    }
+
+    def act(action: (K,V) => Unit): Map[K,V] = {
+      pout.foreach(tp => action(tp._1, tp._2))
+      pout
+    }
   }
 
   private var pendingTupleOps = Set[TupleOpId]()
-
-  trait Region {
-    def withinRegion(p: Point2D): Boolean
-  }
-  case class RectangularRegion(start: Point2D, end: Point2D) extends Region {
-    override def withinRegion(p: Point2D): Boolean =
-      p.x >= start.x && p.x <= end.x && p.y >= start.y && p.y <= end.y
-  }
-  case class CircularRegion(center: Point2D, radius: Double) extends Region {
-    override def withinRegion(p: Point2D): Boolean = Math.sqrt(Math.pow(p.x-center.x, 2) + Math.pow(p.y-center.y, 2)) <= radius
-  }
 
   type Tuple = String
   type TupleTemplate = String
@@ -73,7 +126,8 @@ class TupleBasedCoordination extends AggregateProgram with StandardSensors with 
   }
 
   def tupleOperation(toid: TupleOpId)(arg: ProcArg): (TupleResult, Status) = {
-    node.put(toid.uid, 1)
+    node.put(toid.uid, 1) // marker that a process instance has been executed
+
     val res = toid.op match {
       case outop@OutMe(s, initiator, extension) => OutMeLogic(toid, outop, arg)
       case outop@OutInRegion(s, region) => OutInRegionLogic(toid, outop, arg)
@@ -82,8 +136,8 @@ class TupleBasedCoordination extends AggregateProgram with StandardSensors with 
       case _ => (TupleResult("invalid"), Terminated)
     }
 
-    node.put(toid.uid+"_status", if(res._2==Output) 2 else if(res._1==Bubble) 1 else 0)
-    //println(s"[$mid] $toid -> $res")
+    node.put(toid.uid+"_status", if(res._2==Output) 2 else if(res._2==Bubble) 1 else 0)
+    // println(s"[$mid] $toid -> $res")
     res
   }
 
@@ -123,14 +177,15 @@ class TupleBasedCoordination extends AggregateProgram with StandardSensors with 
     val Read(ttemplate, initiator, extension) = readOp
 
     val g = classicGradient(initiator==mid)
-    val tupleFound = localTupleMatching(localTuples, ttemplate)
-    val result  = broadcastUnbounded[Option[Tuple]](!tupleFound.isEmpty, tupleFound)
+    node.put(s"${toid.uid}_g", g)
+    val tupleFound = solveFirst(ttemplate) // localTupleMatching(localTuples, ttemplate)
+    val result  = broadcastUnbounded[Option[TupleSolution]](!tupleFound.isEmpty, tupleFound)
     val (gotIt,canClose) = rep((false,false))(f => {
       val shouldBeDone = mid==initiator && !result.isEmpty
       (shouldBeDone, f._2 || (shouldBeDone && f._1))
     })
-    (TupleResult(result.getOrElse("not-found")), if(mid==initiator){
-      if(canClose) Terminated else if(gotIt) Output else Bubble
+    (TupleResult(result.map(_.solution).getOrElse("not-found")), if(mid==initiator){
+      if(canClose){ print("#"); Terminated } else if(gotIt) Output else Bubble
     } else if(g<extension) Bubble else External)
   }
 
@@ -147,24 +202,24 @@ class TupleBasedCoordination extends AggregateProgram with StandardSensors with 
     // 4) TupleRemovalEnd
     val g = classicGradient(initiator==mid)
 
-    val tupleFound = localTupleMatching(localTuples, ttemplate)
+    val tupleFound = solveFirst(ttemplate)
     val myTupleChosen = consensusOn(tupleFound, initiator==mid, g)
 
     if(myTupleChosen){
       // Once the device who found the tuple sees that its tuple is the chosen one, it must get confirmation by tuple's owner
       // Tuple removal must affect Out processes
-      emitEvent(TupleRemovalRequested(toid, tupleFound.get))
+      emitEvent(TupleRemovalRequested(toid, tupleFound.get.toString))
     }
 
     val outProcess = events.collectFirst[TupleOpId]{ case TupleRemovalOk(by) => by }
     val gotAck = !outProcess.isEmpty
     val canRetrieveTheTuple = broadcastUnbounded(gotAck, gotAck)
 
-    val result  = broadcastUnbounded[Option[String]](myTupleChosen && canRetrieveTheTuple, tupleFound)
+    val result  = broadcastUnbounded[Option[String]](myTupleChosen && canRetrieveTheTuple, tupleFound.map(_.solution))
 
     val didRead = broadcastUnbounded(mid==initiator, mid==initiator && !result.isEmpty)
     if(myTupleChosen && didRead){ emitEvent(TupleRemovalDone) }
-    val done = broadcastUnbounded(myTupleChosen && didRead, (myTupleChosen && didRead) && localProcs.forall(_!=outProcess))
+    val done = broadcastUnbounded(myTupleChosen && didRead, (myTupleChosen && didRead) && localProcs.forall(lp => outProcess.map(_!=lp).getOrElse(true)))
 
     // IN bubble closing similar to read
     val (gotIt,canClose) = rep((false,false))(f => {
@@ -176,6 +231,7 @@ class TupleBasedCoordination extends AggregateProgram with StandardSensors with 
     } else if(g<extension) Bubble else External)
   }
 
+  /*
   def localTupleMatching(localTuples: Set[Tuple], t: TupleTemplate): Option[Tuple] =
     rep(None:Option[Tuple]) { v =>
       if(!v.isEmpty && localTuples.contains(v.get)) v else localTuples.collectFirst {
@@ -183,14 +239,16 @@ class TupleBasedCoordination extends AggregateProgram with StandardSensors with 
       }
     }
 
+   */
+
   def emitEvent(event: ProcessEvent): Unit = {
     events += event
   }
 
   def broadcastUnbounded[V](source: Boolean, field: V): V =
-    G[V](source, field, v => v, nbrRange)
+    G[V](source, field, v => v, nbrRange _)
 
-  case class ProcArg(localTuples: Set[Tuple], procs: Set[TupleOpId])
+  case class ProcArg(localTuples: Set[Tuple] = Set.empty, procs: Set[TupleOpId] = Set.empty)
 
   def tupleManagement(): Set[String] = {
     // Visualize active process instances after cleaning old ones
@@ -212,6 +270,7 @@ class TupleBasedCoordination extends AggregateProgram with StandardSensors with 
     tuples.values.map(_.datum).toSet
   }
 
+  /*
   def out(data: String, extension: Double, optId: Option[String]) = {
     pendingTupleOps += TupleOpId(tupleProcessPrefix+optId.getOrElse(s"out_${mid}_${k}"))(OutMe(data, mid, extension))
   }
@@ -231,5 +290,5 @@ class TupleBasedCoordination extends AggregateProgram with StandardSensors with 
   def in(data: String, optId: Option[String]) = {
     pendingTupleOps += TupleOpId(tupleProcessPrefix+optId.getOrElse(s"in_${mid}_${k}"))(In(data, mid))
   }
-
+   */
 }
